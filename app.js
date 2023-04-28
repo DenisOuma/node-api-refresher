764c60c6bfba35d3008850539c3d72485c12408a
Denis Ouma <dennisouma38@gmail.com>
2023-04-28 13:55:38 +0300

    Adding routes to the application

--- app.js
+++ app.js
@@ -1,38 +1,10 @@
-const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-	const url = req.url;
-	const method = req.method;
-	if (url === "/") {
-		res.setHeader("Content-Type", "text/html");
-		res.write("<html>");
-		res.write("<head><title>Enter Message</title></head>");
-		res.write(
-			"<body><form action='/message' method='POST'><input type='text' name= 'message'> <button type='submit'>Send</button> </form></body>"
-		);
-		res.write("</html>");
-		return res.end();
-	}
-	if (url === "/message" && method === "POST") {
-		const body = [];
-		req.on("data", (chunk) => {
-			body.push(chunk);
-		});
-		req.on("end", () => {
-			const parsedBody = Buffer.concat(body).toString();
-			const message = parsedBody.split("=")[1];
-			fs.writeFileSync("message.txt", message);
-		});
-		res.statusCode = 302;
-		res.setHeader("Location", "/");
-		return res.end();
-	}
-	res.setHeader("Content-Type", "text/html");
-	res.write("<html>");
-	res.write("<head><title>My First Page</title></head>");
-	res.write("<body><h1>Welcome to BizCoop API</h1></body>");
-	res.write("</html>");
-	res.end();
-});
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
 
-server.listen(5000);
+app.use(bodyParser.urlencoded());
+app.use(adminRoutes);
+app.use(shopRoutes);
+app.listen(3000);
