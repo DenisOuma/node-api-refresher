0796087ab8ffd5540b5a26b101e5f94f7c5793a9
Denis Ouma <dennisouma38@gmail.com>
2023-04-28 14:23:20 +0300

    Exploring the view

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ const shopRoutes = require("./routes/shop.js");
 app.use(bodyParser.urlencoded());
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+// Error handling Page cannot be found
+
+app.use((req, res, next) => {
+	res.status(404).send("<h1>Page not found</h1>");
+});
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-	res.send(
-		"<form action = '/product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
-	);
+	res
+		.status(200)
+		.send(
+			"<form action = '/product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
+		);
 });
 router.post("/product", (req, res, next) => {
 	console.log(req.body);
